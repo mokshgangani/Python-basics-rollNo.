838a493e762609c27c3e3724e5102fd46d5d99eb
mokshgangani <mokshgangani@gmail.com>
2026-09-09 10:37:18 +0530

    program in c++ for factorial user input

---
+++ factorial.py
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+  int fact=1;
+  int n;
+  cout<<"Enter n"<<endl;
+  cin>>n;
+for(int i=n;i<=1;i--)
+{
+  fact=fact*i;
+}
+  cout<<fact;
+}
+  
